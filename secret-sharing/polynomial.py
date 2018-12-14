807ee5aaf270cfdf1e801ef6d748cf639d83906d
David Loughnane <david.loughnane1@gmail.com>
2018-12-14 20:33:06 +0000

    blacken, type annotations

--- secret-sharing/polynomial.py
+++ secret-sharing/polynomial.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+from typing import List, Iterator
 from itertools import zip_longest
 
 
-def strip(L, elt):
-    '''Strip all copies of elt from the end of the list.
+def strip(L: List[int], elt: int) -> List[int]:
+    """Strip all copies of elt from the end of the list.
 
     Arguments:
         L: a list (an indexable, sliceable object)
@@ -10,7 +12,7 @@ def strip(L, elt):
 
     Returns:
         a slice of L with all copies of elt removed from the end.
-    '''
+    """
     if len(L) == 0:
         return L
 
@@ -18,65 +20,67 @@ def strip(L, elt):
     while i >= 0 and L[i] == elt:
         i -= 1
 
-    return L[:i+1]
+    return L[: i + 1]
 
 
-class Polynomial(object):
-    '''A class representing a polynomial as a list of coefficients with no
+class Polynomial:
+    """A class representing a polynomial as a list of coefficients with no
     trailing zeros.
 
     A degree zero polynomial corresponds to the empty list of coefficients,
     and is provided by this module as the variable ZERO.
 
     Polynomials override the basic arithmetic operations.
-    '''
+    """
 
-    def __init__(self, coefficients):
-        '''Create a new polynomial.
+    def __init__(self, coefficients: List[int]) -> None:
+        """Create a new polynomial.
 
         The caller must provide a list of all coefficients of the
         polynomial, even those that are zero. E.g.,
         Polynomial([0, 1, 0, 2]) corresponds to f(x) = x + 2x^3.
-        '''
+        """
         self.coefficients = strip(coefficients, 0)
-        self.indeterminate = 'x'
+        self.indeterminate = "x"
 
-    def add(self, other):
-        newCoefficients = [
-            sum(x) for x in zip_longest(self, other, fillvalue=0.)
-        ]
+    def add(self, other: Polynomial) -> Polynomial:
+        newCoefficients = [sum(x) for x in zip_longest(self, other, fillvalue=0.)]
         return Polynomial(newCoefficients)
 
-    def __add__(self, other):
+    def __add__(self, other: Polynomial) -> Polynomial:
         return self.add(other)
 
-    def multiply(self, other):
+    def multiply(self, other: Polynomial) -> Polynomial:
         newCoeffs = [0] * (len(self) + len(other) - 1)
 
         for i, a in enumerate(self):
             for j, b in enumerate(other):
-                newCoeffs[i+j] += a*b
+                newCoeffs[i + j] += a * b
 
         return Polynomial(strip(newCoeffs, 0))
 
-    def __mul__(self, other):
+    def __mul__(self, other: Polynomial) -> Polynomial:
         return self.multiply(other)
 
-    def __len__(self):
-        '''len satisfies len(p) == 1 + degree(p).'''
+    def __len__(self) -> int:
+        """len satisfies len(p) == 1 + degree(p)."""
         return len(self.coefficients)
 
-    def __repr__(self):
-        return ' + '.join(['%s %s^%d' % (a, self.indeterminate, i) if i > 0 else '%s' % a
-                           for i, a in enumerate(self.coefficients)])
+    def __repr__(self) -> str:
+        return " + ".join(
+            [
+                "%s %s^%d" % (a, self.indeterminate, i) if i > 0 else "%s" % a
+                for i, a in enumerate(self.coefficients)
+            ]
+        )
 
-    def evaluateAt(self, x):
-        '''Evaluate a polynomial at an input point.
+    def evaluateAt(self, x: int) -> int:
+        """Evaluate a polynomial at an input point.
 
         Uses Horner's method, first discovered by Persian mathematician
         Sharaf al-Dīn al-Ṭūsī, which evaluates a polynomial by minimizing
         the number of multiplications.
-        '''
+        """
         theSum = 0
 
         for c in reversed(self.coefficients):
@@ -84,16 +88,16 @@ class Polynomial(object):
 
         return theSum
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator:
         return iter(self.coefficients)
 
-    def __neg__(self):
+    def __neg__(self) -> Polynomial:
         return Polynomial([-a for a in self])
 
-    def __sub__(self, other):
+    def __sub__(self, other: Polynomial) -> Polynomial:
         return self + (-other)
 
-    def __call__(self, *args):
+    def __call__(self, *args) -> int:
         return self.evaluateAt(args[0])
 
 
