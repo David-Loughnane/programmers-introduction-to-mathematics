86ae96826d638a0bdd906225001d251c4c4ba657
David Loughnane <david.loughnane1@gmail.com>
2018-12-18 13:55:25 +0000

    tests are pytest only

--- secret-sharing/polynomial_test.py
+++ secret-sharing/polynomial_test.py
@@ -1,64 +1,58 @@
-from assertpy import assert_that
-
-from polynomial import strip
+from polynomial import strip_zeros
 from polynomial import Polynomial
 
 
 def test_strip_empty():
-    assert_that(strip([], 1)).is_equal_to([])
+    assert strip_zeros([]) == []
 
 
 def test_strip_single():
-    assert_that(strip([1, 2, 3, 1], 1)).is_equal_to([1, 2, 3])
+    assert strip_zeros([1, 2, 3, 1, 0]) == [1, 2, 3, 1]
 
 
 def test_strip_many():
-    assert_that(strip([1, 2, 3, 1, 1, 1, 1], 1)).is_equal_to([1, 2, 3])
-
-
-def test_strip_string():
-    assert_that(strip("123111", "1")).is_equal_to("123")
+    assert strip_zeros([1, 2, 3, 0, 0, 0, 0]) == [1, 2, 3]
 
 
 def test_polynomial_zero():
-    assert_that(Polynomial([0]).coefficients).is_equal_to([])
-    assert_that(len(Polynomial([0]))).is_equal_to(0)
+    assert Polynomial([0]).coefficients == []
+    assert len(Polynomial([0])) == 0
 
 
 def test_polynomial_repr():
     f = Polynomial([1, 2, 3])
-    assert_that(repr(f)).is_equal_to("1 + 2 x^1 + 3 x^2")
+    assert repr(f) == "1 + 2 x^1 + 3 x^2"
 
 
 def test_polynomial_add():
     f = Polynomial([1, 2, 3])
     g = Polynomial([4, 5, 6])
-    assert_that((f + g).coefficients).is_equal_to([5, 7, 9])
+    assert (f + g).coefficients == [5, 7, 9]
 
 
 def test_polynomial_sub():
     f = Polynomial([1, 2, 3])
     g = Polynomial([4, 5, 6])
-    assert_that((f - g).coefficients).is_equal_to([-3, -3, -3])
+    assert (f - g).coefficients == [-3, -3, -3]
 
 
 def test_polynomial_add_zero():
     f = Polynomial([1, 2, 3])
     g = Polynomial([0])
-    assert_that((f + g).coefficients).is_equal_to([1, 2, 3])
+    assert (f + g).coefficients == [1, 2, 3]
 
 
 def test_polynomial_negate():
     f = Polynomial([1, 2, 3])
-    assert_that((-f).coefficients).is_equal_to([-1, -2, -3])
+    assert (-f).coefficients == [-1, -2, -3]
 
 
 def test_polynomial_multiply():
     f = Polynomial([1, 2, 3])
     g = Polynomial([4, 5, 6])
-    assert_that((f * g).coefficients).is_equal_to([4, 13, 28, 27, 18])
+    assert (f * g).coefficients == [4, 13, 28, 27, 18]
 
 
 def test_polynomial_evaluate_at():
     f = Polynomial([1, 2, 3])
-    assert_that((f(2))).is_equal_to(1 + 4 + 12)
+    assert (f(2)) == 1 + 4 + 12
